2ed33d08a7b4d120f4a637f4152dcdc915a6792d
Dewan Sarwar <sarwarcse@gmail.com>
2018-11-04 22:25:00 +1300

    fix up requests package issue

--- src/main.py
+++ src/main.py
@@ -5,18 +5,15 @@ modelEntityList = ['weinstein_1995.cellml#Concentrations.C_ext_Na', 'mackenzie_1
 componentList = []
 
 for index in range(len(modelEntityList)):
-    print(index, modelEntityList[index])
-
     workspaceURL = 'https://models.physiomeproject.org/workspace/267/rawfile/HEAD/'
     modelName = modelEntityList[index][0:modelEntityList[index].find('#')]
-    print(modelName)
     r = requests.get(workspaceURL + modelName)
 
     # extract name of component from a cellml model entity
     componentVariable = modelEntityList[index][modelEntityList[index].find('#') + 1:len(modelEntityList[index])]
     componentName = componentVariable[:componentVariable.find('.')]
 
-    # traverse xml doc
+    # traverse xml dom, find above component name, and pull that including its children
     doc = pulldom.parseString(r.text)
     for event, node in doc:
         if event == pulldom.START_ELEMENT and node.tagName == 'component':
